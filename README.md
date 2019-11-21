d16ca8d52c27f5ba9f15118179cb563fc91b1c7f
billsowa <billsowa@gmail.com>
2019-11-21 13:30:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-cvifma
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-cvifma)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import "./style.scss";
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<div class="de-accordion">Testing123</div>`;
